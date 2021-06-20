45008d6e74e062bf176bc63f6954ac7ce90ec338
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-06-19 21:28:16 -0400

    Code Fix and allow for DataStore Cluster SDRS

--- new-vmharddisk-ontap.ps1
+++ new-vmharddisk-ontap.ps1
@@ -13,22 +13,24 @@
         https://github.com/rebelinux/NetappCode
     #>
 
-$VMs = @('cluster1-01','cluster1-02')
+$VMs = @('cluster1-01')
+$DCLUSTER = "SSD-HIGH-PERF"
 foreach ($VM in $VMs) {
     $scsi = get-vm $VM | Get-ScsiController
     if (!$scsi){ 
         foreach($x in (0..11)) {
             if ($scsi.Name -notlike "SCSI controller 0"){ 
-                Get-VM $VM | New-HardDisk -CapacityGB 0.5 -StorageFormat EagerZeroedThick | New-ScsiController -Type VirtualLsiLogicSAS -BusSharingMode Virtual
+                Get-VM $VM | New-HardDisk -CapacityGB 30 -StorageFormat EagerZeroedThick | New-ScsiController -Type VirtualLsiLogicSAS -BusSharingMode Virtual
                 $scsi = get-vm $VM | Get-ScsiController
                 }
             else {
                 Write-Host "Scsi Adapter Found in VM adding disks number $($x)"
-                Get-VM $VM | New-HardDisk -CapacityGB 0.5 -StorageFormat EagerZeroedThick -Controller "SCSI controller 0" 
-                new-advancedsetting -entity $VM -name "scsi0:$X.sharing" -value "multi-writer" -confirm:$false
-                new-advancedsetting -entity $VM -name "scsi0:$X.virtualSSD" -value 1 -confirm:$false
-                new-advancedsetting -entity $VM -name "scsi0:$X.ctkEnabled" -value "false" -confirm:$false
+                Get-VM $VM | New-HardDisk -CapacityGB 30 -StorageFormat EagerZeroedThick -Controller "SCSI controller 0" -Datastore $DCLUSTER
+                new-advancedsetting -entity $VM -name "scsi0:$X.sharing" -value "multi-writer" -confirm:$false -Force
+                new-advancedsetting -entity $VM -name "scsi0:$X.virtualSSD" -value 1 -confirm:$false -Force
+                new-advancedsetting -entity $VM -name "scsi0:$X.ctkEnabled" -value "false" -confirm:$false -Force
             }
         }
     }
 }
+
